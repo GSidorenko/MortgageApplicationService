3e73a3b28c9012bd08c7df19ca410fabcc57ec26
gsidorenco <gsidorenco@gmail.com>
2021-11-06 00:53:13 +0400

    Initial commit

---
+++ src/main/java/com/example/MortgageApplicationService/MortgageApplicationServiceApp.java
@@ -0,0 +1,18 @@
+package com.example.MortgageApplicationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@OpenAPIDefinition(
+		info = @Info(title = "Mortgage Application Service",description = "API ипотечных заявок", version = "1.0")
+)
+
+@SpringBootApplication
+public class MortgageApplicationServiceApp extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageApplicationServiceApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MortgageApplicationService/MortgageApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MortgageApplicationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
